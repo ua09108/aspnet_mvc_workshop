7f58baa8661b32c563618bedf1529370a7f6138a
ua09108 <ua09108@hotmail.com>
2017-12-04 16:40:34 +0800

    New Function for import excel to sql server 1.Add EF fcuntion 2.Add Usercontroller controller

---
+++ mvc_demo/Controllers/UserController.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using mvc_demo.Models;
+using System.Data.OleDb;
+using System.Data;
+using LinqToExcel;
+using System.Data.Entity.Validation;
+
+namespace mvc_demo.Controllers
+{
+    
+    public class UserController : Controller
+    {
+        private TestdbEntities _db = new TestdbEntities();
+        // GET: User
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public JsonResult UploadExcel(mvc_users users, HttpPostedFileBase FileUpload)
+        {
+
+            List<string> data = new List<string>();
+            if (FileUpload != null)
+            {
+                // tdata.ExecuteCommand("truncate table OtherCompanyAssets");  
+                if (FileUpload.ContentType == "application/vnd.ms-excel" || FileUpload.ContentType == "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet")
+                {
+
+
+                    string filename = FileUpload.FileName;
+                    string targetpath = Server.MapPath("~/testdata/");
+                    FileUpload.SaveAs(targetpath + filename);
+                    string pathToExcelFile = targetpath + filename;
+                    var connectionString = "";
+                    if (filename.EndsWith(".xls"))
+                    {
+                        connectionString = string.Format("Provider=Microsoft.Jet.OLEDB.4.0; data source={0}; Extended Properties=Excel 8.0;", pathToExcelFile);
+                    }
+                    else if (filename.EndsWith(".xlsx"))
+                    {
+                        connectionString = string.Format("Provider=Microsoft.ACE.OLEDB.12.0;Data Source={0}; Extended Properties=\"Excel 12.0 Xml; HDR=YES; IMEX=1\";", pathToExcelFile);
+                        //connectionString = string.Format("Provider=Microsoft.ACE.OLEDB.12.0;Data Source={0};Extended Properties=\"Excel 12.0 Xml;HDR=YES;IMEX=1\";", pathToExcelFile);
+                    }
+
+                    var adapter = new OleDbDataAdapter("SELECT * FROM [工作表1$]", connectionString);
+                    var ds = new DataSet();
+
+                    adapter.Fill(ds, "ExcelTable");
+
+                    DataTable dtable = ds.Tables["ExcelTable"];
+
+                    string sheetName = "工作表1";
+
+                    var excelFile = new ExcelQueryFactory(pathToExcelFile);
+                    var artistAlbums = from a in excelFile.Worksheet<mvc_users>(sheetName) select a;
+
+                    foreach (var a in artistAlbums)
+                    {
+                        try
+                        {
+                            if (a.Name != "" && a.Address != "" && a.ContactNo != "")
+                            {
+                                mvc_users TU = new mvc_users();
+                                TU.Name = a.Name;
+                                TU.Address = a.Address;
+                                TU.ContactNo = a.ContactNo;
+                                _db.mvc_users.Add(TU);
+
+                                _db.SaveChanges();
+
+
+
+                            }
+                            else
+                            {
+                                data.Add("<ul>");
+                                if (a.Name == "" || a.Name == null) data.Add("<li> name is required</li>");
+                                if (a.Address == "" || a.Address == null) data.Add("<li> Address is required</li>");
+                                if (a.ContactNo == "" || a.ContactNo == null) data.Add("<li>ContactNo is required</li>");
+
+                                data.Add("</ul>");
+                                data.ToArray();
+                                return Json(data, JsonRequestBehavior.AllowGet);
+                            }
+                        }
+
+                        catch (DbEntityValidationException ex)
+                        {
+                            foreach (var entityValidationErrors in ex.EntityValidationErrors)
+                            {
+
+                                foreach (var validationError in entityValidationErrors.ValidationErrors)
+                                {
+
+                                    Response.Write("Property: " + validationError.PropertyName + " Error: " + validationError.ErrorMessage);
+
+                                }
+
+                            }
+                        }
+                    }
+                    //deleting excel file from folder  
+                    //if ((System.IO.File.Exists(pathToExcelFile)))
+                    //{
+                    //    System.IO.File.Delete(pathToExcelFile);
+                    //}
+                    return Json("success", JsonRequestBehavior.AllowGet);
+                }
+                else
+                {
+                    //alert message for invalid file format  
+                    data.Add("<ul>");
+                    data.Add("<li>Only Excel file format is allowed</li>");
+                    data.Add("</ul>");
+                    data.ToArray();
+                    return Json(data, JsonRequestBehavior.AllowGet);
+                }
+            }
+            else
+            {
+                data.Add("<ul>");
+                if (FileUpload == null) data.Add("<li>Please choose Excel file</li>");
+                data.Add("</ul>");
+                data.ToArray();
+                return Json(data, JsonRequestBehavior.AllowGet);
+            }
+        }
+
+    }
+}
