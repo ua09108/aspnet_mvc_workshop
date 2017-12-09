3efd6b649d5c9cf19ac1a5409481ef17f57ecc17
ua09108 <ua09108@hotmail.com>
2017-12-09 12:18:40 +0800

    Test jQuery send object data to MVC.

---
+++ mvc_demo/Controllers/AjaxPostController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace mvc_demo.Controllers
+{
+    public class AjaxPostController : Controller
+    {
+        // GET: AjaxPost
+        public ActionResult Index(string ViewName)
+        {
+            if (string.IsNullOrEmpty(ViewName))
+            { return View(); }
+            else
+            {
+                return View(ViewName);
+            }
+
+        }
+
+        public ActionResult Update(List<Player> players)
+        {
+            return Json(players);
+        }
+
+        public class Player
+        {
+            public string Id { get; set; }
+            public string Name { get; set; }
+        }
+    }
+}
---
+++ mvc_demo/Controllers/BootBoxController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace mvc_demo.Controllers
+{
+    public class BootBoxController : Controller
+    {
+        // GET: BootBox
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult BootBox()
+        {
+            return View();
+        }
+    }
+}
